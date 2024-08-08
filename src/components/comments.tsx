fc0ecd0142c0aa1e417b14f425aa7f783abad5bc
Best <Photchara_k@cmu.ac.th>
2024-08-08 15:21:14 +0700

    123546

--- src/components/comments.tsx
+++ src/components/comments.tsx
@@ -1,18 +1,15 @@
+
 "use client";
 
 
 export default function Comment({
-  userImagePath,
-  username,
-  commentText,
-  likeNum,
-  replies,
+
  }) {
   return (
     <div>
       <div className="d-flex gap-2 my-2">
           <img
-            src="/profileImages/lisa.jpg"
+            src="/profileImages/charliebrown.jpg"
             width="48"
             height="48"
             className="rounded-circle"
@@ -23,17 +20,15 @@ export default function Comment({
             style={{ backgroundColor: "#3A3B3C" }}
           >
             <span className="fw-semibold" style={{ color: "#E4E6EB" }}>
-              Lisa
+              Charlie Brown
             </span>
             <br />
-            <span style={{ color: "#E4E6EB" }}>จริงค่า</span>
+            <span style={{ color: "#E4E6EB" }}>บ้าไปแล้ว</span>
             <div className="d-flex align-items-center gap-1">
-              <img src="/like.svg" width={20}></img>
-              <span style={{ color: "#B0B3B8" }}>999 คน</span>
+              
             </div>
           </div>
         </div>
-
 
         
       {/* You can use map-loop to render Reply component here */}
